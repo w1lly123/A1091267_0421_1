8ef900428e2b5e84e46bfb047c8ec11c07f9032e
w1lly123 <125645223+w1lly123@users.noreply.github.com>
2023-05-05 00:27:11 +0800

    Update A1091267_0421_1.java

--- A1091267_0421_1.java
+++ A1091267_0421_1.java
@@ -6,11 +6,7 @@ public class A1091267_0421_1 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("請輸入電子郵件地址: ");
         String email = scanner.nextLine();
-
-        // 定義正則表示式
         String regex = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$";
-
-        // 進行正則表達式匹配
         Pattern pattern = Pattern.compile(regex);
         if (pattern.matcher(email).matches()) {
             System.out.println("電子郵件地址格式正確。");
